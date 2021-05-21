e7228f4b513cc839926899dcb49fdb20d459c10c
sarthak <agarwalsarthak99@gmail.com>
2021-05-21 14:43:22 +0530

    Second commit

--- client/src/components/Pages/Classroom/SubPages/Classroom1.js
+++ client/src/components/Pages/Classroom/SubPages/Classroom1.js
@@ -1,10 +1,130 @@
-import React from 'react';
-import ClassroomNavbar from '../ClassroomNavbar';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import * as styles from './styles.module.css'
 
-const Classroom1 =()=>{
-    return(
-        <div>
-            classroom1
+
+const Classroom1 = () => {
+
+    const [state, setState] = useState({
+        title: '',
+        body: '',
+        posts: []
+    });
+
+    useEffect(() => {
+        getBlogPost();
+    }, []);
+
+    const getBlogPost = () => {
+        axios.get('/api')
+            .then((response) => {
+                const data = response.data;
+                const reverseData = new Array;
+                for (let i = data.length - 1; i >= 0; i--) {
+                    reverseData.push(data[i]);
+                }
+
+                setState((previousData) => {
+                    return {
+                        ...previousData,
+                        posts: reverseData
+                    };
+                });
+
+                console.log('Data has been recieved!!');
+            })
+            .catch(() => {
+                alert('Error retrieving data!!');
+            });;
+
+    }
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+
+        setState((prevData) => {
+            return {
+                ...prevData,
+                [name]: value
+            };
+        });
+    }
+
+    const submit = (event) => {
+        event.preventDefault();
+
+        const payload = {
+            title: state.title,
+            body: state.body
+        };
+
+        axios({
+            url: '/api/save',
+            method: 'POST',
+            data: payload
+        })
+            .then(() => {
+                console.log('Data has been sent to the server');
+                resetUserInputs();
+                getBlogPost();
+            })
+            .catch(() => {
+                console.log('Internal server error');
+            });;
+    };
+
+    const resetUserInputs = () => {
+        setState((previousDataEntry) => {
+            return {
+                ...previousDataEntry,
+                title: '',
+                body: ''
+            };
+        });
+    };
+
+    const displayBlogPost = (posts) => {
+        if (!posts.length) return null;
+
+        return posts.map((post, index) => (
+            <div key={index} className={styles['blog-post-display']}>
+                <h3>{post.title}</h3>
+                <p>{post.body}</p>
+            </div>
+        ));
+    };
+
+
+    console.log(state);
+
+
+
+    return (
+        <div className={styles.app}>
+            <h2>welcome folks!! This is clone for classroom</h2>
+            <form className={styles.form} onSubmit={submit}>
+                <div className={styles['form-input']}>
+                    <input
+                        type="text"
+                        name="title"
+                        placeholder="Title"
+                        value={state.title}
+                        onChange={handleChange} />
+                </div>
+                <div className={styles['form-input']}>
+                    <textarea
+                        placeholder="Body"
+                        name="body"
+                        col="30"
+                        rows="10"
+                        value={state.body}
+                        onChange={handleChange} />
+                </div>
+                <button className={styles.btn}>Submit</button>
+                <div className="blog-post">
+                    {displayBlogPost(state.posts)}
+                </div>
+            </form>
         </div>
     );
 
